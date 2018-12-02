5dad884abd830f612376fd2ef62933e6a55efae6
Oscar Nylander <oscar@hedvig.com>
2018-12-02 09:40:07 +0100

    Improve day1 with a generator and some code simplification

--- src/day1.rs
+++ src/day1.rs
@@ -1,17 +1,21 @@
 use std::collections::HashSet;
 
+#[aoc_generator(day1)]
+pub fn parse_day1(input: &str) -> Vec<i32> {
+    input.lines().map(|s| s.parse().unwrap()).collect()
+}
+
 #[aoc(day1, part1)]
-pub fn solve_day1(input: &str) -> i32 {
-    input.lines().map(|s| s.parse::<i32>().unwrap()).sum()
+pub fn solve_day1_part1(input: &[i32]) -> i32 {
+    input.iter().sum()
 }
 
 #[aoc(day1, part2)]
-pub fn solve_day2(input: &str) -> i32 {
-    let mut results: HashSet<i32> = HashSet::new();
-    let input: Vec<i32> = input.lines().map(|s| s.parse::<i32>().unwrap()).collect();
+pub fn solve_day1_part2(input: &[i32]) -> i32 {
+    let mut results: HashSet<i32> = Default::default();
     let mut state = 0;
     loop {
-        for i in &input {
+        for i in input {
             state += i;
             if !results.insert(state) {
                 return state;
